a1cfb29c9e514edfdaff14e53c894f1ba21250c0
jahidcs <csq.jahid@gmail.com>
2023-01-24 16:59:20 +0600

    Nav 1

---
+++ lib/views/nav_1.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white10,
+      appBar: AppBar(
+        title: const Text('NAV 1'),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Container(
+              width: size.width,
+              height: 80,
+              // color: Colors.red,
+              child: Stack(
+                children: [
+                  CustomPaint(
+                    size: Size(size.width, 80),
+                    painter: BNBCustomPainter(),
+                  ),
+                  Center(
+                    heightFactor: 0.8,
+                    child: FloatingActionButton(
+                      onPressed: () {},
+                      backgroundColor: Colors.orange,
+                      child: Icon(Icons.note),
+                      elevation: 0.1,
+                    ),
+                  ),
+                  Container(
+                    width: size.width,
+                    height: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: () => print("Home"),
+                          icon: const Icon(Icons.home),
+                        ),
+                        IconButton(
+                          onPressed: () => print('Menu'),
+                          icon: const Icon(Icons.menu),
+                        ),
+                        SizedBox(width: size.width * 0.20),
+                        IconButton(
+                          onPressed: () => print('Person'),
+                          icon: const Icon(Icons.person),
+                        ),
+                        IconButton(
+                          onPressed: () => print('Bookmark'),
+                          icon: const Icon(Icons.bookmark),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BNBCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    //TODO: implement Paint
+    Paint paint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    Path path = Path()..moveTo(0, 20);
+    path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
+    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
+    path.arcToPoint(Offset(size.width * 0.60, 20),
+        radius: Radius.circular(10.0), clockwise: false);
+
+    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    canvas.drawShadow(path, Colors.black, 5, true);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
